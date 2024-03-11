ad75564af4b66a068386ce1a8e3beffd9a143942
Kamolus02 <s28115@pjwstk.edu.pl>
2024-03-11 13:08:17 +0100

    Pierwsza wersja projektu

---
+++ Zajecia1/Zajecia1/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string [] args)
+    {
+        int i = 2;
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(i);
+    }
+}
